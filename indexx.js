7e0621d8ed0135cb39df329ebf49e83ee03a92c4
Honchar007 <54860596+Honchar007@users.noreply.github.com>
2020-09-03 15:07:35 +0300

    Update indexx.js
    The first part of laba 1 done!

--- indexx.js
+++ indexx.js
@@ -1,1 +1,50 @@
 console.log("Hello");
+
+function arr(number)
+{
+	var ar= [];
+	for (var i = 0; i < number; i++)
+	{
+		ar.push(i);
+	}
+	console.log(ar);
+	return ar;
+}
+
+function nacelo(number,delitel1,delitel2)
+{
+	if (number % delitel1 == 0 && number % delitel2 == 0)
+    {
+      console.log("true");
+      return true;
+    }
+  else
+    {
+      console.log("false");
+      return false;
+    }
+}
+
+var array = arr(5);
+array = arr(9);
+
+var proverka = nacelo(9,3,1);
+var proverka = nacelo(5,3,1);
+
+
+
+function capitalize(word)
+{
+  if(word.length >10)
+  {
+    console.log("Строка содержит больше 10 символов");
+    return ;
+  }
+  var i = word[0];
+ word = word.replace(i, i.toUpperCase());
+ 
+  console.log(word);
+
+}
+capitalize('hiiiii');
+capitalize('hiiiiiiiiiiiiiiiiiii');
